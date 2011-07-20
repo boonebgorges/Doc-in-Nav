d536af130df44d5ec507604e63b4c02366503aac
Boone Gorges <boonebgorges@gmail.com>
2011-07-20 10:15:38 -0400

    Fixes for BP 1.3 compatibility

--- includes/din-functions.php
+++ includes/din-functions.php
@@ -3,8 +3,14 @@
 function din_add_nav_item(){
 	global $bp, $groups_template;
 	
-	if ( $bp->current_component != $bp->groups->slug )
-		return false;
+	if ( function_exists( 'bp_is_groups_component' ) ) {
+		// BP 1.3 support
+		if ( !bp_is_groups_component() )
+			return false;
+	} else {
+		if ( !$bp->groups->slug != $bp->current_component )
+			return false;
+	}
 	
 	if ( !class_exists('BP_Docs_Query'))
 		return false;
@@ -25,21 +31,33 @@ function din_add_nav_item(){
 
 	$din_posts = query_posts($args);
 	
+	// In BP 1.3, bp_options_nav for groups is keyed by group slug instead of by 'groups', to
+	// differentiate it from the top-level groups directories and the groups subtab of member
+	// profiles
+	$group_slug = isset( $bp->groups->current_group->slug ) ? $bp->groups->current_group->slug : false;
+	
+	// If this is not a group page, bail
+	if ( !$group_slug )
+		return false;
+	
+	// Now check to see what the parent nav item should be
+	$parent_nav = isset( $bp->bp_options_nav[$group_slug] ) ? $group_slug : $bp->groups->slug;
+	
 	// create custom Doc in Nav item
 	foreach ( $din_posts as $post ) {
 
-		$bp->bp_options_nav['groups'][$post->post_name]['name'] = $post->post_title;
-		$bp->bp_options_nav['groups'][$post->post_name]['slug'] = $post->post_name;
-		$bp->bp_options_nav['groups'][$post->post_name]['link'] = bp_docs_get_group_doc_permalink($post->ID);
-		$bp->bp_options_nav['groups'][$post->post_name]['css_id'] = 'doc_in_nav';
-		$bp->bp_options_nav['groups'][$post->post_name]['position'] = 90;
-		$bp->bp_options_nav['groups'][$post->post_name]['user_has_access'] = 1;
+		$bp->bp_options_nav[$parent_nav][$post->post_name]['name'] = $post->post_title;
+		$bp->bp_options_nav[$parent_nav][$post->post_name]['slug'] = $post->post_name;
+		$bp->bp_options_nav[$parent_nav][$post->post_name]['link'] = bp_docs_get_group_doc_permalink($post->ID);
+		$bp->bp_options_nav[$parent_nav][$post->post_name]['css_id'] = 'doc_in_nav';
+		$bp->bp_options_nav[$parent_nav][$post->post_name]['position'] = 90;
+		$bp->bp_options_nav[$parent_nav][$post->post_name]['user_has_access'] = 1;
 	
 	}
 	wp_reset_query();
 	
 }
-add_action( 'wp', 'din_add_nav_item' );
+add_action( 'bp_setup_nav', 'din_add_nav_item' );
 
 	
 
@@ -48,7 +66,7 @@ add_action('admin_menu', 'din_create_menu');
 	
 function din_create_menu() {
 
-	if ( !is_site_admin() )
+	if ( !is_super_admin() )
 		return false;
 
 	if ( !class_exists('BP_Docs_Query'))
